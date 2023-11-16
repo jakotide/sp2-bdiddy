2d8c4390ed0a648c8b6af58c50e9ae0dcaa4172d
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2023-11-16 21:44:54 +0100

    Fixed vite config for multiple pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from "vite";
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-    base: '/'
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'pages/login.html'),
+      },
+    },
+  },
+});
