bf833dd1560fb97173c2e51dd4d009dccdfca4ee
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2023-11-16 22:02:19 +0100

    Test

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    base: '/sp2-bdiddy/', 
+  base: "/sp2-bdiddy/",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'pages/login.html'),
+        main: resolve(__dirname, "index.html"),
+        login: resolve(__dirname, "pages/login.html"),
       },
     },
   },
