31a40dc13eb0f6fbe8967057eb90b1ca6c985ca5
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2023-11-29 12:42:57 +0100

    Updated vite.config

---
+++ assets/login-60715d93.js
@@ -0,0 +1,1 @@
+import"./main-cb7af1f5.js";const t=document.querySelectorAll(".login-toggle"),o=document.querySelector(".main-login");t.forEach(e=>{e.addEventListener("click",()=>{o.classList.toggle("active")})});
--- assets/login-d5408d2c.js
+++
@@ -1,1 +0,0 @@
-import"./main-aab10879.js";const t=document.querySelectorAll(".login-toggle"),o=document.querySelector(".main-login");t.forEach(e=>{e.addEventListener("click",()=>{o.classList.toggle("active")})});
