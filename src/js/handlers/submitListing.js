a4766581556497c6474adaa1568109d92f72af82
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2023-12-05 15:01:31 +0100

    Finished submit listing js

--- src/js/handlers/submitListing.js
+++ src/js/handlers/submitListing.js
@@ -13,67 +13,48 @@ export async function submitListing() {
       const endsAt = `${date}T${time}`;
       const tagsInput = document.getElementById("tags");
       const tagsContainer = document.querySelector(".tags-container");
+      const createBtn = document.querySelector(".create-btn");
 
       const listing = {
         title: formData.get("title"),
         description: formData.get("description"),
-        tags: formData.getAll("tags"),
         endsAt: endsAt,
       };
+
+      const tags = Array.from(tagsContainer.children)
+        .filter((tagElement) => tagElement.classList.contains("tag"))
+        .map((tagElement) =>
+          tagElement.querySelector(".tag-text").textContent.trim()
+        )
+        .filter((tag) => tag !== "");
+
+      console.log(tags);
+
+      if (tags.length > 0) {
+        listing.tags = tags;
+      }
 
       const modalForm = document.querySelector(".upload-images-form");
       if (modalForm) {
         const modalFormData = new FormData(modalForm);
         const mediaUrls = Array.from(modalFormData.getAll("media"));
+        const nonEmptyMediaUrls = mediaUrls.filter((url) => url.trim() !== "");
 
-        if (mediaUrls.length > 0) {
-          listing.media = mediaUrls;
+        if (nonEmptyMediaUrls.length > 0) {
+          listing.media = nonEmptyMediaUrls;
         }
       }
 
       try {
+        createBtn.innerHTML = '<span class="loader"></span>';
         await createListing(listing);
-        console.log("Success!");
+        createBtn.textContent = "Success!";
+        // setTimeout(() => {
+
+        // }, 2000);
       } catch (error) {
         console.error("Error creating listing:", error);
       }
     });
   }
 }
-// import { createListing } from "../api/listings";
-
-// export async function submitListing() {
-//   const form = document.querySelector(".create-form");
-
-//   if (form) {
-//     form.addEventListener("submit", async (e) => {
-//       e.preventDefault();
-//       const form = e.target;
-//       const formData = new FormData(form);
-//       const date = formData.get("date");
-//       const time = formData.get("time");
-//       const endsAt = `${date}T${time}`;
-
-//       const listing = {
-//         title: formData.get("title"),
-//         description: formData.get("description"),
-//         tags: formData.get("tags").split(","),
-//         endsAt: endsAt,
-//       };
-
-//       // const modalForm = document.querySelector(".upload-images-form");
-//       // if (modalForm) {
-//       //   const modalFormData = new FormData(modalForm);
-//       //   const mediaUrls = Array.from(modalFormData.getAll("media"));
-//       //   listing.media = mediaUrls;
-//       // }
-
-//       try {
-//         await createListing(listing);
-//         console.log("Success!");
-//       } catch (error) {
-//         console.error("Error creating listing:", error);
-//       }
-//     });
-//   }
-// }
