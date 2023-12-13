8f34834184bf0d0ceb7add28aa5dfe69491dddf7
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2023-12-13 17:17:21 +0100

    Finished contact form submission

--- src/js/pages/contact.js
+++ src/js/pages/contact.js
@@ -1,14 +1,34 @@
-import '../../scss/main.scss';
+import "../../scss/main.scss";
 
-import '../components/hamburger';
-import { authorizeToken } from '../storage/authorizeToken';
+import "../components/hamburger";
+import { authorizeToken } from "../storage/authorizeToken";
 const createLink = document.querySelector("#createBtn");
+const submitBtn = document.querySelector(".create-btn");
 
 authorizeToken(
-    () => {
-        createLink.style.display = "block";
-    },
-    () => {
-        createLink.style.display = "none";
-    }
-  );
+  () => {
+    createLink.style.display = "block";
+  },
+  () => {
+    createLink.style.display = "none";
+  }
+);
+
+document.getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    showSuccessMessage();
+  });
+
+function showSuccessMessage() {
+  const successMessage = document.getElementById("successMessage");
+  submitBtn.innerHTML = `<span class="loader"></span>`
+
+  setTimeout(() => {
+    successMessage.style.display = "block";
+    document.getElementById("name").value = "";
+    document.getElementById("title").value = "";
+    document.getElementById("description").value = "";
+    submitBtn.innerText = "Success";
+  }, 1000);
+}
