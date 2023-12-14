b54f2346ff786ab783fe4298996e944dc622df75
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2023-12-14 10:45:43 +0100

    Minor style changes for safari mobile

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -2,6 +2,8 @@ import { API_AUCTION_URL } from "../constants";
 import { AUTH_REGISTER } from "../constants";
 const loginBtn = document.getElementById("signUpBtn");
 const main = document.querySelector(".main-login");
+const errorMsg = document.querySelector(".reg-error-msg");
+
 
 export async function register(profile) {
   const regUrl = API_AUCTION_URL + AUTH_REGISTER;
@@ -18,7 +20,8 @@ export async function register(profile) {
     if (!response.ok) {
       const errorData = await response.json();
       console.error("Registration failed:", errorData);
-      alert("Something went wrong with the registration. Try another username.");
+      errorMsg.style.display = "block";
+      gsap.to(loginBtn, { x: -4, yoyo: true, repeat: 3, duration: 1 });
     } else {
       loginBtn.innerHTML = '<span class="loader"></span>';
 
@@ -30,6 +33,5 @@ export async function register(profile) {
       }, 2000);
     }
   } catch (error) {
-    console.error("An error occurred during registration:", error);
   }
 }
