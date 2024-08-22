c87b0f3cc660dfef0c992b63beffe30f7de4eb52
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-08-22 11:23:28 +0200

    Testing routes

--- vite.config.js
+++ vite.config.js
@@ -22,11 +22,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        login: resolve(__dirname, "sp2-bdiddy/authentication/index.html"),
-        create: resolve(__dirname, "sp2-bdiddy/create/index.html"),
-        profile: resolve(__dirname, "sp2-bdiddy/profile/index.html"),
-        listing: resolve(__dirname, "sp2-bdiddy/listing/index.html"),
-        contact: resolve(__dirname, "sp2-bdiddy/contact/index.html"),
+        login: resolve(__dirname, "authentication/index.html"),
+        create: resolve(__dirname, "create/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+        listing: resolve(__dirname, "listing/index.html"),
+        contact: resolve(__dirname, "contact/index.html"),
       },
     },
   },
