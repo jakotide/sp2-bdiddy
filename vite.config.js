c4eb6d829b87e0111f1772e3cb141d6cce231f0e
Jakob Tidemand <111381232+jakotide@users.noreply.github.com>
2024-08-22 10:00:56 +0200

    Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
+// export default defineConfig({
+//   base: "/sp2-bdiddy/",
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, "index.html"),
+//         login: resolve(__dirname, "authentication/index.html"),
+//         create: resolve(__dirname, "create/index.html"),
+//         profile: resolve(__dirname, "profile/index.html"),
+//         listing: resolve(__dirname, "listing/index.html"),
+//         contact: resolve(__dirname, "contact/index.html")
+//       },
+//     },
+//   },
+// });
+
 export default defineConfig({
-  base: "/sp2-bdiddy/",
   build: {
     rollupOptions: {
       input: {
@@ -11,9 +26,8 @@ export default defineConfig({
         create: resolve(__dirname, "create/index.html"),
         profile: resolve(__dirname, "profile/index.html"),
         listing: resolve(__dirname, "listing/index.html"),
-        contact: resolve(__dirname, "contact/index.html")
+        contact: resolve(__dirname, "contact/index.html"),
       },
     },
   },
 });
-
